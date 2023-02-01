2d0c8ffe6d35c71746cd4c360d7dbde2cca8d682
oodinc <masoodinc1@gmail.com>
2023-02-01 20:06:44 +0700

    revisi1

--- js/script.js
+++ js/script.js
@@ -6,9 +6,28 @@ menu.onclick = () =>{
     navbar.classList.toggle('active');
 }
 
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
 window.onscroll = () =>{
+
     menu.classList.remove('fa-th-list');
     navbar.classList.remove('active');
+
+    section.forEach(sec =>{
+
+      let top = window.scrollY;
+      let height = sec.offsetHeight;
+      let offset = sec.offsetTop - 150;
+      let id = sec.getAttribute('id');
+
+      if(top >= offset && top < offset + height){
+        navLinks.forEach(links =>{
+          links.classList.remove('active');
+          document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+        });
+      };
+    });
 }
 
 document.querySelector('#search-icon').onclick = () =>{
@@ -18,3 +37,51 @@ document.querySelector('#search-icon').onclick = () =>{
 document.querySelector('#close').onclick = () =>{
     document.querySelector('#search-form').classList.remove('active');
 }
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5500,
+      disableOnInteraction: false,
+    },
+    loop:true,
+    
+    breakpoints: {
+      0:{
+        slidesPerView: 1,
+      },
+      640:{
+        slidesPerView: 2,
+      },
+      768:{
+        slidesPerView: 2,
+      },
+      912:{
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+  }
+
+  function fadeOut(){
+    setInterval(loader, 3000)
+  }
+  window.onload = fadeOut;
